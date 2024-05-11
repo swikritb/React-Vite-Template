37717639577938bb4ff1130f262efa17f096c769
swikritbta <swikrit@ramailo.tech>
2024-05-11 15:17:59 +0545

    Initial Setup Vite-Typescript-Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+     <div className="flex justify-center item-center bg-gray-100">
+      <h1 className=" text-3xl font-bold underline">Hello world!</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
